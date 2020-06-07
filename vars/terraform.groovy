f6bd7b4d6b25ef72e5b22235086d5032de61b14e
Chandralekha.s <chandralekha.s@sophos.com>
2020-06-07 19:27:10 +0530

    Updating jenkins-shared-library module

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -27,8 +27,8 @@ def call(){
                   export AWS_ACCESS_KEY_ID="${AWS_USR}"
                   export AWS_SECRET_ACCESS_KEY="${AWS_PSW}"
                   export AWS_DEFAULT_REGION="us-west-2"
-                  export TF_VAR_LICENSE_KEY = ${LICENSE_KEY}
-                  export TF_VAR_API_KEY = ${API_KEY}
+                  export TF_VAR_LICENSE_KEY = $LICENSE_KEY
+                  export TF_VAR_API_KEY = $API_KEY
                   terraform get -update
                   terraform init
               '''
@@ -48,8 +48,8 @@ def call(){
                   export AWS_ACCESS_KEY_ID="${AWS_USR}"
                   export AWS_SECRET_ACCESS_KEY="${AWS_PSW}"
                   export AWS_DEFAULT_REGION="us-west-2"
-                  export TF_VAR_LICENSE_KEY = ${LICENSE_KEY}
-                  export TF_VAR_API_KEY = ${API_KEY}
+                  export TF_VAR_LICENSE_KEY = $LICENSE_KEY
+                  export TF_VAR_API_KEY = $API_KEY
                   terraform apply -auto-approve
               '''
                 }
@@ -68,8 +68,8 @@ def call(){
                   export AWS_ACCESS_KEY_ID="${AWS_USR}"
                   export AWS_SECRET_ACCESS_KEY="${AWS_PSW}"
                   export AWS_DEFAULT_REGION="us-west-2"
-                  export TF_VAR_LICENSE_KEY = ${LICENSE_KEY}
-                  export TF_VAR_API_KEY = ${API_KEY}
+                  export TF_VAR_LICENSE_KEY = $LICENSE_KEY
+                  export TF_VAR_API_KEY = $API_KEY
                   terraform destroy -auto-approve
               '''
                 }
